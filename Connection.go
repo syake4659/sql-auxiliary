2aee109eb840913a1068f5831169f83bd1130f94
syake <syake4659@gmail.com>
2020-11-29 14:38:54 +0900

    Table syntax generation
    update: Table syntax generation
    update: Supports Primary Key

--- Connection.go
+++ Connection.go
@@ -50,6 +50,7 @@ type ColumnData struct {
 type TableData struct {
 	Name    string
 	Columns []ColumnData
+	DBName  string
 }
 
 // DataType is SQL DataType
@@ -62,6 +63,7 @@ type DataType struct {
 	MaxLength      int
 	DefaultPropaty string
 	AutoIncrement  bool
+	PrimaryKey     bool
 }
 
 func contains(list []string, target string) bool {
@@ -75,67 +77,67 @@ func contains(list []string, target string) bool {
 
 var (
 	// TINYINT is the one that exists in SQL and can store numbers from -128 to 127. If you add UNSIGNED, you can store up to 255, but you can only use integers.
-	TINYINT = DataType{TypeName: "TINYINT", Type: "INT8", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT8", AutoIncrement: true}
+	TINYINT = DataType{TypeName: "TINYINT", Type: "INT8", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT8", AutoIncrement: true, PrimaryKey: true}
 
 	// SMALLINT is the one that exists in SQL and can store numbers from -32768 to 32767. With UNSIGNED, you can store up to 65535, but you can only use integers.
-	SMALLINT = DataType{TypeName: "SMALLINT", Type: "INT16", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT16", AutoIncrement: true}
+	SMALLINT = DataType{TypeName: "SMALLINT", Type: "INT16", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT16", AutoIncrement: true, PrimaryKey: true}
 
 	// MEDIUMINT is the one that exists in SQL and can store numbers from -8388608 to 8388607. With UNSIGNED, you can store up to 16777215, but you can only use integers.
-	MEDIUMINT = DataType{TypeName: "MEDIUMINT", Type: "INT32", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT32", AutoIncrement: true}
+	MEDIUMINT = DataType{TypeName: "MEDIUMINT", Type: "INT32", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT32", AutoIncrement: true, PrimaryKey: true}
 
 	// INT is the one that exists in SQL and can store numbers from -2147483648 to 2147483647. With UNSIGNED, you can store up to 4294967295, but you can only use integers.
-	INT = DataType{TypeName: "INT", Type: "INT32", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT32", AutoIncrement: true}
+	INT = DataType{TypeName: "INT", Type: "INT32", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT32", AutoIncrement: true, PrimaryKey: true}
 
 	// INTEGER is the one that exists in SQL and can store numbers from -2147483648 to 2147483647. With UNSIGNED, you can store up to 4294967295, but you can only use integers.
-	INTEGER = DataType{TypeName: "INTEGER", Type: "INT32", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT32", AutoIncrement: true}
+	INTEGER = DataType{TypeName: "INTEGER", Type: "INT32", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT32", AutoIncrement: true, PrimaryKey: true}
 
 	// BIGINT is the one that exists in SQL and can store numbers from -9223372036854775808 to 9223372036854775807. With UNSIGNED, you can store up to 18446744073709551615, but you can only use integers.
-	BIGINT = DataType{TypeName: "BIGINT", Type: "INT64", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT64", AutoIncrement: true}
+	BIGINT = DataType{TypeName: "BIGINT", Type: "INT64", UNSIGNED: true, ZEROFILL: true, UndignedType: "UNIT64", AutoIncrement: true, PrimaryKey: true}
 
 	// BOOLEAN is a type in SQL that can store True or False.
-	BOOLEAN = DataType{TypeName: "BOOLEAN", Type: "BOOL", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false}
+	BOOLEAN = DataType{TypeName: "BOOLEAN", Type: "BOOL", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false, PrimaryKey: true}
 
 	// BOOL is a type in SQL that can store True or False.
-	BOOL = DataType{TypeName: "BIGINT", Type: "BOOL", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false}
+	BOOL = DataType{TypeName: "BIGINT", Type: "BOOL", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false, PrimaryKey: false}
 
 	// BIT is a SQL type that can store bit values.
-	BIT = DataType{TypeName: "UNIT", Type: "UNIT", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false}
+	BIT = DataType{TypeName: "UNIT", Type: "UNIT", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false, PrimaryKey: true}
 
 	// FLOAT is a type that exists in SQL and can store accurate decimals up to the 7th decimal place.
-	FLOAT = DataType{TypeName: "FLOAT", Type: "FLOAT32", UNSIGNED: true, ZEROFILL: true, AutoIncrement: true}
+	FLOAT = DataType{TypeName: "FLOAT", Type: "FLOAT32", UNSIGNED: true, ZEROFILL: true, AutoIncrement: true, PrimaryKey: true}
 
 	// DOUBLE is a type that exists in SQL and can store accurate decimals up to the 7th decimal place.
-	DOUBLE = DataType{TypeName: "DOUBLE", Type: "FLOAT64", UNSIGNED: true, ZEROFILL: true, AutoIncrement: true}
+	DOUBLE = DataType{TypeName: "DOUBLE", Type: "FLOAT64", UNSIGNED: true, ZEROFILL: true, AutoIncrement: true, PrimaryKey: true}
 
 	// DATE is a type that exists in SQL. You can save the year, month, and day.
-	DATE = DataType{TypeName: "DATE", Type: "DATE", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false}
+	DATE = DataType{TypeName: "DATE", Type: "DATE", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false, PrimaryKey: true}
 
 	// DATETIME is a type that exists in SQL. You can save the year, month, day, hour, minute, and second.
-	DATETIME = DataType{TypeName: "DATETIME", Type: "DATE", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false}
+	DATETIME = DataType{TypeName: "DATETIME", Type: "DATE", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false, PrimaryKey: true}
 
 	// TIMESTAMP is a type that exists in SQL. You can save the year, month, day, hour, minute, and second. Also, if no value is explicitly assigned, the date and time will be set automatically when the value is changed.
-	TIMESTAMP = DataType{TypeName: "TIMESTAMP", Type: "DATE", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false}
+	TIMESTAMP = DataType{TypeName: "TIMESTAMP", Type: "DATE", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false, PrimaryKey: true}
 
 	// TIME is a type that exists in SQL. You can save hours, minutes, and seconds.
-	TIME = DataType{TypeName: "TIME", Type: "DATE", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false}
+	TIME = DataType{TypeName: "TIME", Type: "DATE", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false, PrimaryKey: true}
 
 	// VARCHAR is a SQL type. You can store the specified character string.
-	VARCHAR = DataType{TypeName: "VARCHAR", Type: "STRING", UNSIGNED: false, ZEROFILL: false, MaxLength: 65535, DefaultPropaty: "255", AutoIncrement: false}
+	VARCHAR = DataType{TypeName: "VARCHAR", Type: "STRING", UNSIGNED: false, ZEROFILL: false, MaxLength: 65535, DefaultPropaty: "255", AutoIncrement: false, PrimaryKey: false}
 
 	// TEXT is a SQL type. You can store the specified character string.
-	TEXT = DataType{TypeName: "TEXT", Type: "STRING", UNSIGNED: false, ZEROFILL: false, MaxLength: 14090025, DefaultPropaty: "255", AutoIncrement: false}
+	TEXT = DataType{TypeName: "TEXT", Type: "STRING", UNSIGNED: false, ZEROFILL: false, MaxLength: 14090025, DefaultPropaty: "255", AutoIncrement: false, PrimaryKey: false}
 
 	// MIDIUMTEXT is a SQL type. You can store the specified character string.
-	MIDIUMTEXT = DataType{TypeName: "MIDIUMTEXT", Type: "STRING", UNSIGNED: false, ZEROFILL: false, MaxLength: 3741318945, DefaultPropaty: "255", AutoIncrement: false}
+	MIDIUMTEXT = DataType{TypeName: "MIDIUMTEXT", Type: "STRING", UNSIGNED: false, ZEROFILL: false, MaxLength: 3741318945, DefaultPropaty: "255", AutoIncrement: false, PrimaryKey: false}
 
 	// LONGTEXT is a SQL type. You can store the specified character string.
-	LONGTEXT = DataType{TypeName: "LONGTEXT", Type: "STRING", UNSIGNED: false, ZEROFILL: false, MaxLength: 4294967295, DefaultPropaty: "255", AutoIncrement: false}
+	LONGTEXT = DataType{TypeName: "LONGTEXT", Type: "STRING", UNSIGNED: false, ZEROFILL: false, MaxLength: 4294967295, DefaultPropaty: "255", AutoIncrement: false, PrimaryKey: false}
 
 	// ENUM is a type of SQL. Can store one of the specified string lists.
-	ENUM = DataType{TypeName: "TEXT", Type: "LIST", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false}
+	ENUM = DataType{TypeName: "TEXT", Type: "LIST", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false, PrimaryKey: false}
 
 	// SET is a type of SQL. Can store multiple in the specified string list.
-	SET = DataType{TypeName: "SET", Type: "LIST", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false}
+	SET = DataType{TypeName: "SET", Type: "LIST", UNSIGNED: false, ZEROFILL: false, AutoIncrement: false, PrimaryKey: false}
 )
 
 func (err *Error) Error() string {
@@ -219,6 +221,46 @@ func (col *ColumnData) setDefault(value interface{}) *ColumnData {
 func Column(name string, dataType DataType) *ColumnData {
 	column := ColumnData{Name: name, DataType: dataType}
 	return &column
+}
+
+func (tab TableData) build() (string, error) {
+	autoIncrement := false
+	columns := tab.Columns
+	name := tab.Name
+	dbname := tab.DBName
+	primarys := []string{}
+	uniqueIndex := []string{}
+	result := fmt.Sprintf("CREATE TABLE '%v'.'%v' (", dbname, name)
+	for _, i := range columns {
+		if i.AutoIncremental {
+			if autoIncrement {
+				return "", &Error{Msg: "Up to one AutoIncrement can be set for each table."}
+			}
+			autoIncrement = true
+		}
+		if i.PrimaryKey {
+			primarys = append(primarys, i.Name)
+		}
+		if i.UniqueIndex {
+			uniqueIndex = append(uniqueIndex, i.Name)
+		}
+		r, err := i.build()
+		if err != nil {
+			return "", err
+		}
+		result += r
+	}
+	if len(primarys) >= 1 {
+		result += ","
+		result += fmt.Sprintf("PRIMARY KEY (%v)", toSQLList(primarys))
+	}
+	if len(uniqueIndex) >= 1 {
+		for _, i := range uniqueIndex {
+			result += ","
+			result += fmt.Sprintf("UNIQUE INDEX `%v_UNIQUE` (`%v` ASC) VISIBLE", i, i)
+		}
+	}
+	return result, nil
 }
 
 func (col ColumnData) build() (string, error) {
